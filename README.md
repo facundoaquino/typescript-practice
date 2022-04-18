9593c62cdb5986d7a7646ac0b4eb53d92252fc61
Facundo Aquino <facun90@live.com>
2022-04-17 21:58:36 -0300

    tsconfig and readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# TYPESCRIPT 🤓💾
+
+`tsc --init ` to create a tsconfig.json
+
+`tsc --w ` watch a generate .js files transpiled
---
+++ bases/app.ts
@@ -0,0 +1,10 @@
+let msg: string = 'hi friends im here'
+console.log(msg)
+
+const obj = {
+	name: 'facundo',
+	age: 39,
+}
+
+obj.age = '9'
+console.log(typeof obj.age)
