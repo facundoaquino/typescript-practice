a205387c553e695415cac2960d17666bda751e65
Facundo Aquino <facun90@live.com>
2022-04-25 16:59:44 -0300

    functions args rest

--- functions/app.js
+++ functions/app.js
@@ -1,2 +1,12 @@
 ;
-(function () { })();
+(function () {
+    var fullName = function (firstName) {
+        var restArgs = [];
+        for (var _i = 1; _i < arguments.length; _i++) {
+            restArgs[_i - 1] = arguments[_i];
+        }
+        return "".concat(firstName, "  ").concat(restArgs.join(' '));
+    };
+    var fully = fullName('fede', 'berton');
+    console.log(fully);
+})();
