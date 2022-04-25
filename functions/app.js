08afd2f6ae4fdf2306c4a8bc72080612c8705c7a
Facundo Aquino <facun90@live.com>
2022-04-25 17:11:36 -0300

    type function 2

--- functions/app.js
+++ functions/app.js
@@ -1,9 +1,2 @@
 ;
-(function () {
-    var addNumber = function (a, b) { return a + b; };
-    var greet = function (name) { return "hello ".concat(name); };
-    var saveWorld = function () { return 'The world is ok'; };
-    var myfunction;
-    var myfunction2;
-    myfunction2 = addNumber;
-})();
+(function () { })();
--- functions/app.ts
+++ functions/app.ts
@@ -1,13 +1,1 @@
-;(() => {
-	const addNumber = (a: number, b: number) => a + b
-
-	const greet = (name: string) => `hello ${name}`
-
-	const saveWorld = () => 'The world is ok'
-
-	let myfunction: Function
-
-	let myfunction2: (y: number, g: number) => number
-
-	myfunction2 = addNumber
-})()
+;(() => {})()
