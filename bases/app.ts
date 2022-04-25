942b15aefff731f63c34c6c2e4af77c66a99587c
Facundo Aquino <facun90@live.com>
2022-04-25 07:53:20 -0300

    numbers

--- bases/app.ts
+++ bases/app.ts
@@ -1,13 +1,1 @@
-;(() => {
-	let avangers = 10
-
-	console.log(avangers)
-
-	const villians: number = 20
-
-	if (avangers < villians) {
-		console.log('we are in problems')
-	} else {
-		console.log('saftly')
-	}
-})()
+ 
---
+++ bases/numbers.ts
@@ -0,0 +1,13 @@
+;(() => {
+	let avangers: number = 10
+
+	console.log(avangers)
+
+	const villians: number = 20
+
+	if (avangers < villians) {
+		console.log('we are in problems')
+	} else {
+		console.log('saftly')
+	}
+})()
