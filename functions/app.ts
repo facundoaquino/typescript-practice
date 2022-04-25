b18782f2c74dcaf9351392eb86250efdf8db83e8
Facundo Aquino <facun90@live.com>
2022-04-25 17:00:19 -0300

    refactor

--- functions/app.ts
+++ functions/app.ts
@@ -1,9 +1,1 @@
-;(() => {
-	const fullName = (firstName: string, ...restArgs: string[]): string => {
-		return `${firstName}  ${restArgs.join(' ')}`
-	}
-
-	const fully = fullName('fede', 'berton')
-
-	console.log(fully)
-})()
+;(() => {})()
