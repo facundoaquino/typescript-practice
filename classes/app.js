4f99f10dca087a3c9fcb7a5c82832ccae0c9b996
Facundo Aquino <facun90@live.com>
2022-06-20 10:02:29 -0300

    classes methods

--- classes/app.js
+++ classes/app.js
@@ -1,17 +1,22 @@
 ;
 (function () {
     var Avenger = /** @class */ (function () {
-        // shorthand to create a class
         function Avenger(name, team, realName) {
             this.name = name;
             this.team = team;
             this.realName = realName;
         }
+        Avenger.getAvgAge = function () {
+            return this.name;
+        };
+        // BY DEFECT METHODS ARE PUBLIC
+        Avenger.prototype.bio = function () {
+            return "".concat(this.name, "  (").concat(this.team, ")");
+        };
         Avenger.avgAge = 35;
         return Avenger;
     }());
     var antman = new Avenger('AntMan', 'Capital', 'scoty land');
-    console.log(antman);
-    // we can call de statics properties without instance the class
-    console.log(Avenger.avgAge);
+    console.log(antman.bio());
+    console.log(Avenger.getAvgAge());
 })();
