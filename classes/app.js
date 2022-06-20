f0317cc6174852d3bda6581df86d8e17834e5a86
Facundo Aquino <facun90@live.com>
2022-06-20 10:59:28 -0300

    classes private constructor SINGLETON

--- classes/app.js
+++ classes/app.js
@@ -1,43 +1,25 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 ;
 (function () {
-    var Mutante = /** @class */ (function () {
-        function Mutante(name, realName) {
+    //  Constructor privado: no se usa mucho , es para controlar como son ejecutadas nuestras instancias , puede usarse para manejar singletons
+    // SINGLETON!!
+    var Apocalipsis = /** @class */ (function () {
+        function Apocalipsis(name) {
             this.name = name;
-            this.realName = realName;
         }
-        return Mutante;
-    }());
-    var Xmen = /** @class */ (function (_super) {
-        __extends(Xmen, _super);
-        function Xmen() {
-            return _super !== null && _super.apply(this, arguments) || this;
-        }
-        Xmen.prototype.salvarMundo = function () {
-            console.log('Salvando mundo...');
+        Apocalipsis.callApocalipsis = function () {
+            if (!Apocalipsis.intance) {
+                Apocalipsis.intance = new Apocalipsis('Soy Apocalipsis unicooo');
+            }
+            return Apocalipsis.intance;
         };
-        return Xmen;
-    }(Mutante));
-    var Villian = /** @class */ (function (_super) {
-        __extends(Villian, _super);
-        function Villian() {
-            return _super !== null && _super.apply(this, arguments) || this;
-        }
-        return Villian;
-    }(Mutante));
-    var wolverine = new Xmen('wolverine', 'logan');
+        return Apocalipsis;
+    }());
+    // const apocalipsis1 = new Apocalipsis('Soy apoca the one')
+    // const apocalipsis2 = new Apocalipsis('Soy apoca the one')
+    // const apocalipsis3 = new Apocalipsis('Soy apoca the one')
+    // console.log(apocalipsis1)
+    // console.log(apocalipsis2)
+    // console.log(apocalipsis3)
+    var apocalipsis1 = Apocalipsis.callApocalipsis();
+    console.log(apocalipsis1);
 })();
